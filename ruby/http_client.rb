354f0718f21ffb29566c575d29369ac609517cda
Pedro Bahamondes <pibahamondesw@gmail.com>
2023-09-03 18:36:00 -0300

    Add content type to HTTP Client
    Just to be compliant with docs and ensure use of JSON parsers

--- ruby/http_client.rb
+++ ruby/http_client.rb
@@ -3,13 +3,14 @@
 # Simple HTTP Client
 class HTTPClient
   class << self
-    def http_get(url, authorization = nil, params = nil)
+    def http_get(url, authorization = nil, params = nil, content_type = 'application/json')
       uri = URI.parse(url)
       uri.query = URI.encode_www_form(params) unless params.nil?
 
       request = Net::HTTP::Get.new(uri)
       request['User-Agent'] = 'Mozilla/5.0'
       request['Authorization'] = authorization unless authorization.nil?
+      request['Content-Type'] = content_type
       Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
         http.request(request)
       end
