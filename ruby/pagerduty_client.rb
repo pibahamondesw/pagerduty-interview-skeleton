d927f18000da7193042c2efb1d9893b487cb03e7
Pedro Bahamondes <pibahamondesw@gmail.com>
2023-09-03 18:27:41 -0300

    Make PagerDutyClient specific to PagerDuty
    Use token auth, PD URL, and custom log

--- ruby/pagerduty_client.rb
+++ ruby/pagerduty_client.rb
@@ -1,31 +1,44 @@
-require "json"
-require "net/http"
+# frozen_string_literal: true
 
+require 'json'
+require 'net/http'
+require_relative 'http_client'
+
+# PagerDuty Client skeleton for interview
 class PagerdutyClient
-  class << self
-    def http_get(url)
-      uri = URI(url)
+  BASE_URL = 'https://api.pagerduty.com'
+  DEBUG = ENV.fetch('DEBUG', nil) == 'true'
 
-      request = Net::HTTP::Get.new(uri)
-      request["User-Agent"] = "Mozilla/5.0"
+  def initialize
+    set_token
+  end
 
-      puts "\nSending 'GET' request to URL : #{url}"
-      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-        http.request(request)
-      }
+  def http_get(endpoint, params = nil)
+    url = "#{BASE_URL}/#{endpoint}"
+    authorization = "Token token=#{@pd_token}"
+    log "Sending 'GET' request to URL : #{url}"
+    response = HTTPClient.http_get(url, authorization, params)
+    log "Response Code : #{response.code}"
+    log JSON.pretty_generate(json_to_map(response.body))
+    response
+  end
 
-      puts "Response Code : #{response.code}"
-      response.body
-    end
+  def json_to_map(json_string)
+    JSON.parse(json_string)
+  end
 
-    def json_to_map(json_string)
-      JSON.parse(json_string)
-    end
+  def input_line
+    $stdin.gets.chomp
+  end
 
-    def get_input_line()
-      STDIN.gets().chomp()
-    end
+  private
+
+  def set_token
+    @pd_token = ENV.fetch('PD_TOKEN', nil)
+    raise 'You need to set the environment variable "PD_TOKEN" to use this Client' if @pd_token.nil?
+  end
+
+  def log(info)
+    puts info if DEBUG
   end
 end
-
-puts "This is where the execution code can be added"
